ded307a8a9bc5076b174c566a4405d58b3001c9d
a114221044-ui <a114221044@mail.shu.edu.tw>
2026-04-30 15:47:09 +0800

    114_04_30

---
+++ 115_4_30/Chap8/Tutorial 8-5/Phonebook/Phonebook/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Phonebook
+{
+    struct PhoneBookEntry
+    {
+        public string name;
+        public string phone;
+    }
+
+    public partial class Form1 : Form
+    {
+        // Field to hold a list of PhoneBookEntry objects.
+        private List<PhoneBookEntry> phoneList = 
+            new List<PhoneBookEntry>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // The ReadFile method reads the contents of the
+        // PhoneList.txt file and stores it as PhoneBookEntry
+        // objects in the phoneList.
+        private void ReadFile()
+        {
+            try
+            {
+                StreamReader inputFile;
+                string line;
+                char[] delim = { ',' };
+                PhoneBookEntry entry = new PhoneBookEntry();
+
+                using (inputFile = File.OpenText("PhoneList.txt"))
+                {
+                    while (!inputFile.EndOfStream)
+                    {
+                        line = inputFile.ReadLine();
+                        string[] fields = line.Split(delim);
+                        if (fields.Length == 2)
+                        {
+                            entry.name = fields[0];
+                            entry.phone = fields[1];
+                            phoneList.Add(entry);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("讀取檔案時發生錯誤: " + ex.Message);
+            }
+        }
+
+        // The DisplayNames method displays the list of names
+        // in the namesListBox control.
+        private void DisplayNames()
+        {
+            foreach (PhoneBookEntry entry in phoneList)
+            {
+                nameListBox.Items.Add(entry.name);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+          ReadFile();
+
+            DisplayNames();
+        }
+
+        private void nameListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int index = nameListBox.SelectedIndex;
+            if (index != -1)
+            {
+                PhoneBookEntry entry = phoneList[index];
+                phoneLabel .Text = entry.phone;
+            }
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            // Close the form.
+            this.Close();
+        }
+    }
+}

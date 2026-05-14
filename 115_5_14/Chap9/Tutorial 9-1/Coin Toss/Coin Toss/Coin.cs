b91083edc877ec374f1a4aec6159c10949a55299
a114221044-ui <a114221044@mail.shu.edu.tw>
2026-05-14 15:58:38 +0800

    1150514

---
+++ 115_5_14/Chap9/Tutorial 9-1/Coin Toss/Coin Toss/Coin.cs
@@ -0,0 +1,8 @@
+﻿namespace Coin_Toss
+{
+     class Coin
+    {
+        private string sideUp;
+        private string sideDown;
+    }
+}
---
+++ 115_5_14/Chap9/Tutorial 9-3/Account Simulator/Account Simulator/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Account_Simulator
+{
+    public partial class Form1 : Form
+    {
+        private BankAccount account = new BankAccount(20000m);
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            balanceLabel.Text = account.Balance.ToString("C");
+
+        }
+
+        private void depositButton_Click(object sender, EventArgs e)
+        {
+          decimal depositAmount;
+            if (decimal.TryParse(depositTextBox.Text, out depositAmount))
+            {
+                account.Deposit(depositAmount);
+                balanceLabel.Text = account.Balance.ToString("C");
+                depositTextBox.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Please enter a valid decimal number for the deposit amount.");
+            }
+        }
+
+        private void withdrawButton_Click(object sender, EventArgs e)
+        {
+            decimal withdrawAmount;
+            if (decimal.TryParse(withdrawTextBox.Text, out withdrawAmount))
+            {
+                account.Withdraw(withdrawAmount);
+                balanceLabel.Text = account.Balance.ToString("C");
+                withdrawTextBox.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Please enter a valid decimal number for the withdraw amount.");
+            }
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            // Close the form.
+            this.Close();
+        }
+    }
+}
